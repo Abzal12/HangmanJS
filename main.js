7f0133fcb94897e35c49896daa5a20256c919ea7
hyper <hyperskill@hyperskill.org>
2024-01-14 11:45:57 +0600

    Stage 5/8

--- main.js
+++ main.js
@@ -5,11 +5,31 @@ const input = require('sync-input')
 const words = ['python', 'java', 'swift', 'javascript'];
 let index = Math.floor(Math.random() * words.length);
 let rightWord = words[index];
-let hint = rightWord.slice(0, 3) + '-'.repeat(rightWord.length - 3);
 
-let guessingWord = input(`Guess the word ${hint}: `);
-if (guessingWord === rightWord) {
-    console.log("You survived!");
-} else {
-    console.log("You lost!");
+console.log("H A N G M A N");
+console.log();
+let rightWordWithHyphens = "-".repeat(rightWord.length);
+let rightWordWithHyphensArr = rightWordWithHyphens.split("");
+let attempts = 8;
+while (attempts > 0) {
+    rightWordWithHyphens = rightWordWithHyphensArr.join("");
+    console.log(rightWordWithHyphens);
+    let guessingLetter = input('Input a letter: ');
+    attempts--;
+
+
+    if (rightWordWithHyphens.includes(guessingLetter)) {
+
+    } else if (rightWord.includes(guessingLetter)) {
+        for (let i = 0; i < rightWord.length; i++) {
+            if (rightWord[i] === guessingLetter) {
+                rightWordWithHyphensArr[i] = guessingLetter;
+            }
+        }
+    } else if (!rightWord.includes(guessingLetter)) {
+        console.log("That letter doesn't appear in the word.");
+    }
+    console.log();
 }
+
+console.log("Thanks for playing!");
