e211522c71b5d143370dcae7b9b6dc72568021e6
hyper <hyperskill@hyperskill.org>
2024-01-14 13:36:52 +0600

    Stage 7/8

--- main.js
+++ main.js
@@ -12,18 +12,22 @@ let rightWordWithHyphens = "-".repeat(rightWord.length);
 let rightWordWithHyphensArr = rightWordWithHyphens.split("");
 let attempts = 8;
 let hasFinished = false;
+let guessedLetters = [];
 while (attempts > 0 && !hasFinished) {
 
     console.log(rightWordWithHyphens);
     let guessingLetter = input('Input a letter: ');
 
-    // if (guessingLetter === "" || guessingLetter === /\s/) {
-    //     console.log("Thanks for playing!");
-    //     hasFinished = true;
-    // }
-    if (rightWordWithHyphens.includes(guessingLetter)) {
-        --attempts;
-        console.log("No improvements.");
+    if (guessingLetter.length !== 1) {
+        console.log("Please, input a single letter.");
+        continue;
+    } else if (!(/[a-z]/.test(guessingLetter))) {
+        console.log("Please, enter a lowercase letter from the English alphabet.");
+        continue;
+    }
+
+    if (rightWordWithHyphens.includes(guessingLetter) || guessedLetters.includes(guessingLetter)) {
+        console.log("You've already guessed this letter.");
     } else if (rightWord.includes(guessingLetter)) {
         for (let i = 0; i < rightWord.length; i++) {
             if (rightWord[i] === guessingLetter) {
@@ -33,13 +37,14 @@ while (attempts > 0 && !hasFinished) {
     } else if (!rightWord.includes(guessingLetter)) {
         attempts--;
         console.log("That letter doesn't appear in the word.");
+        guessedLetters.push(guessingLetter);
     }
     rightWordWithHyphens = rightWordWithHyphensArr.join("");
 
     if (!rightWordWithHyphens.includes("-")) {
         console.log();
         console.log(rightWordWithHyphens);
-        console.log("You guessed the word!");
+        console.log("You guessed the word " + rightWord + "!");
         console.log("You survived!")
         console.log("Thanks for playing!");
         hasFinished = true;
